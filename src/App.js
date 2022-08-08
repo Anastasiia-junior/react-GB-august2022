2077c96de4e29abc19d45f1318c01de2b88e2972
Anastasia <anastasia.tuschina@yandex.ru>
2022-08-08 16:01:30 +0300

    выполнила первые три пункта ДЗ

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import './App.css';
-import { Message } from './Message/Message';
 
-let text = 'Hello, React'
 
 function App() {
+  const [messageList, setMessageList] = useState(
+    [{text: 'dsdavds', author: 'user'}, 
+    {text: 'vjsdbjnj sdhnc jnd', author: 'admin'}]);
+
+  const [value, setValue] = useState('');
+
+  const clickHandler = () => {
+    setMessageList([...messageList, {text: value, author: 'user'}]);
+    setValue('');
+  }
+
+  const changeHandler = (event) => {
+    setValue(event.target.value);
+  }
+
   return (
-    <div className="App">
-     <Message text={text}></Message>
-    </div>
+    <React.Fragment >
+      {messageList.map((message) => <div>{message.author}:{message.text}</div>)}
+     
+      <textarea type='text'  value={value} onChange={changeHandler} className='textarea'></textarea>
+      <button type='button'  onClick={clickHandler} className='send-message' >отправить</button>
+    </React.Fragment>
   );
 }
 
