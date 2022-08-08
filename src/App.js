8e9c49e8da5157e90879844f7c55b8f659132c7b
Anastasia <anastasia.tuschina@yandex.ru>
2022-08-08 17:09:06 +0300

    доделала 4 и 5 задание ко второму уроку

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
   const [messageList, setMessageList] = useState(
-    [{text: 'dsdavds', author: 'user'}, 
-    {text: 'vjsdbjnj sdhnc jnd', author: 'admin'}]);
+    [{ text: 'dsdavds', author: 'user' },
+    { text: 'vjsdbjnj sdhnc jnd', author: 'bot' }]);
 
   const [value, setValue] = useState('');
 
+  useEffect(() => {
+    if (messageList[messageList.length - 1].author === 'user') {
+      setTimeout(() => {
+        setMessageList([...messageList, { text: 'answer from bot', author: 'bot' }])
+      }, 2000)
+    }
+  }, [messageList])
+
   const clickHandler = () => {
-    setMessageList([...messageList, {text: value, author: 'user'}]);
+    setMessageList([...messageList, { text: value, author: 'user' }]);
     setValue('');
   }
 
@@ -20,10 +28,10 @@ function App() {
 
   return (
     <React.Fragment >
-      {messageList.map((message) => <div>{message.author}:{message.text}</div>)}
-     
-      <textarea type='text'  value={value} onChange={changeHandler} className='textarea'></textarea>
-      <button type='button'  onClick={clickHandler} className='send-message' >отправить</button>
+      {messageList.map((message) => <div className='message'><span className='author'>{message.author}:</span>{message.text}</div>)}
+
+      <textarea type='text' value={value} onChange={changeHandler} className='textarea'></textarea>
+      <button type='button' onClick={clickHandler} className='send-message' >отправить</button>
     </React.Fragment>
   );
 }
