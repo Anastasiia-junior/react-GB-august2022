f4281bedea7d74ac029cfe3e4c02ef983f71650f
Anastasia <anastasia.tuschina@yandex.ru>
2022-08-10 17:51:33 +0300

    1. убрала ошибку с setInterval. 2. установила material-ui 3. проставила ключи для списка сообщений

--- src/App.js
+++ src/App.js
@@ -3,34 +3,55 @@ import './App.css';
 
 
 function App() {
-  const [messageList, setMessageList] = useState(
-    [{ text: 'dsdavds', author: 'user' },
-    { text: 'vjsdbjnj sdhnc jnd', author: 'bot' }]);
+  const [messageList, setMessageList] = useState([]);
 
   const [value, setValue] = useState('');
 
   useEffect(() => {
-    if (messageList[messageList.length - 1].author === 'user') {
-      setTimeout(() => {
-        setMessageList([...messageList, { text: 'answer from bot', author: 'bot' }])
-      }, 2000)
-    }
-  }, [messageList])
+
+    let timerId = null;
+
+    if (messageList.length && messageList[messageList.length - 1].author === 'user') {
+
+      timerId = setTimeout(() => {
+        setMessageList([...messageList, { text: 'answer from bot', author: 'bot',  date: new Date()}])
+      }, 2000);
+    };
+
+    return () => {
+      clearInterval(timerId)
+    };
+  }, [messageList]);
 
   const clickHandler = () => {
-    setMessageList([...messageList, { text: value, author: 'user' }]);
+    if(value) {
+      setMessageList([...messageList, { text: value, author: 'user', date: new Date() }]);
     setValue('');
+    }
   }
 
   const changeHandler = (event) => {
     setValue(event.target.value);
   }
 
+  const handlePressInput = ({code}) =>{
+    if (code === 'Enter'){
+      clickHandler();
+    }
+  }
+
   return (
     <React.Fragment >
-      {messageList.map((message) => <div className='message'><span className='author'>{message.author}:</span>{message.text}</div>)}
+      {messageList.map((message, index) => <div className='message' key={index}>
+        <span className='author'>{message.author}:</span>{message.text}
+        <p>21:08</p>
+        </div>)}
 
-      <textarea type='text' value={value} onChange={changeHandler} className='textarea'></textarea>
+      <textarea type='text' 
+                value={value} 
+                onChange={changeHandler} 
+                onKeyPress={handlePressInput}
+                className='textarea'></textarea>
       <button type='button' onClick={clickHandler} className='send-message' >отправить</button>
     </React.Fragment>
   );
