d32c4cb8eb4bf237ddaa39dfdf69264ccd4011fc
Anastasia <anastasia.tuschina@yandex.ru>
2022-09-07 18:45:13 +0300

    Добавила middleware для ответа бота как на уроке

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,21 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import {profileReducer} from './profile-reducer';
 import {chatReducer} from './chat-reducer';
 import { dialogsReducer } from './dialogs-reducer';
+import { botMessage } from "./middlewares";
 
 export const store = createStore( combineReducers ( {
     profile: profileReducer,
     chat: chatReducer,
     dialogs: dialogsReducer
-} ));
+} ), 
+compose(
+    applyMiddleware(botMessage), 
+    window.__REDUX_DEVTOOLS_EXTENSION__ 
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (arg) => arg
+    )
+);
 
 
 window.state = store;
